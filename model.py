9baa32ccbf22f4aad714d1db86f1cd90a7ed9529
JEFFREY ZHANG <jozhang@berkeley.edu>
2017-07-17 19:23:08 -0700

    Split model into different parts

--- model.py
+++ model.py
@@ -16,16 +16,24 @@ class Model:
 
         self.state_shape = observation_space.shape
         self.states = tf.placeholder(dtype=tf.float32, shape=(None, self.state_shape[0], self.state_shape[1], self.state_shape[2]))
+        self.model_function()
+        self.predict_function()
+        self.lose_function()
+        self.update_function()
+        self.summary_function()
 
+    def model_function(self):
         # TODO make the model CNN
-        # Why does it always go to one completely?
         self.max_pooled_states = tf.nn.max_pool(self.states, ksize=[1,7,7,1], strides=[1,2,2,1], padding='VALID')
+
+    def predict_function(self):
         n, x, y, c = self.max_pooled_states.get_shape().as_list()
         self.states_reshaped = tf.reshape(self.max_pooled_states, [-1, x*y*c])
         self.Q_list = tf.nn.softmax(tf.matmul(self.states_reshaped, self.W))
         self.maxQ = tf.reduce_max(self.Q_list, axis=1)  # how did they pick best action as opposed to max Q ... idk if right axis
         self.predictions = tf.cast(tf.argmax(self.Q_list, axis=1), tf.int32)
 
+    def lose_function(self):
         self.actions = tf.placeholder(dtype=tf.int32, shape=(None,))
         self.rewards = tf.placeholder(dtype=tf.int32, shape=(None,))
         self.next_states = tf.placeholder(dtype=tf.float32, shape=(None, self.state_shape[0], self.state_shape[1], self.state_shape[2]))
@@ -35,6 +43,7 @@ class Model:
         self.Q = tf.reduce_sum(tf.multiply(self.Q_list, self.num_actions), axis=1)
         self.targetQ = tf.placeholder(shape=[None], dtype=tf.float32)
 
+    def update_function(self):
         # Batch gradient descent update
         self.regularization_loss = self.REGULARIZATION_COEFF * tf.norm(self.W)
         self.simple_loss = tf.reduce_mean(tf.nn.l2_loss(self.targetQ - self.Q))
@@ -42,11 +51,11 @@ class Model:
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.LEARNING_RATE)
         self.train_step = self.optimizer.minimize(self.loss)
 
+    def summary_function(self):
         tf.summary.scalar("Regularization loss", self.regularization_loss)
         tf.summary.scalar("Simple loss", self.simple_loss)
         tf.summary.scalar("Total loss", self.loss)
         self.merged_summaries = tf.summary.merge_all()
-
 
 
     def update_learning_rate(self, multiplicative_factor):
