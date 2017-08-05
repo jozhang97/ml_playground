f40e4159f4b398170676e247739f460f52950f69
JEFFREY ZHANG <jozhang@berkeley.edu>
2017-08-05 01:45:40 -0700

    Tracking weights and using small std

--- helper/network_helper.py
+++ helper/network_helper.py
@@ -1,12 +1,18 @@
 import tensorflow as tf
 import math
+fc_count = 1
+cnn_count = 1
+
 
 def add_weight_loss(W):
     weight_loss = tf.nn.l2_loss(W, name="weight_loss")
     tf.add_to_collection('weight_losses', weight_loss)
 
+
 def apply_cnn_layer(x, shape, constant=0.1, stride=3, stddev=0.1):
-    W = tf.Variable(tf.truncated_normal(shape, stddev=stddev))
+    a, b, c, d = shape
+    stddev = math.sqrt(2.0 / b * c * d)  # see delving deep into rectifiers by msr
+    W = create_cnn(shape, stddev)
     b = tf.Variable(tf.constant(constant, shape=(shape[3],)))
     conv = tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding='SAME')
     ret = tf.nn.bias_add(conv, b)
@@ -27,7 +33,7 @@ def apply_fc_layer(x, output_size, constant=0.0, stddev=0.1):
         stddev = math.sqrt(2.0/b*c*d)  # see delving deep into rectifiers by msr
     input_size = x.get_shape().as_list()[1]
     # multiply out
-    W = tf.Variable(tf.truncated_normal([input_size, output_size], stddev))
+    W = create_fc(input_size, output_size, stddev)
     b = tf.Variable(tf.constant(constant, shape=(output_size,)))
     multiplied = tf.matmul(x, W)
     added_bias = tf.nn.bias_add(multiplied, b)
@@ -35,3 +41,21 @@ def apply_fc_layer(x, output_size, constant=0.0, stddev=0.1):
     relu = tf.nn.relu(added_bias)
     add_weight_loss(W)
     return relu
+
+
+def create_fc(input_size, output_size, stddev):
+    W = tf.Variable(tf.truncated_normal([input_size, output_size], stddev))
+    global fc_count
+    tf.summary.histogram("weight_fc/" + str(fc_count), W)
+    tf.summary.scalar("weight_fc/" + str(fc_count) + "/mean", tf.reduce_mean(W))
+    fc_count += 1
+    return W
+
+
+def create_cnn(shape, stddev):
+    W = tf.Variable(tf.truncated_normal(shape, stddev=stddev))
+    global cnn_count
+    tf.summary.histogram("weight_cnn/" + str(cnn_count), W)
+    tf.summary.scalar("weight_cnn/" + str(cnn_count) + "/mean", tf.reduce_mean(W))
+    cnn_count += 1
+    return W
